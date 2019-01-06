b146a3633e5626fb25965e901ec7884451231613
chyileee <chyileee@gmail.com>
2019-01-06 21:59:08 +0800

    Add  ClassLibrary_Lee.cs Class origin

--- DotNet_LibraryTutorial/Server_TCP_ok/ClassLibrary_Lee/Server/Networking/Packets/Message.cs
+++ DotNet_LibraryTutorial/Server_TCP_ok/ClassLibrary_Lee/Server/Networking/Packets/Message.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Server.Networking.Packets
+﻿namespace Server.Networking.Packets
 {
     public class Message : PacketStructure
     {

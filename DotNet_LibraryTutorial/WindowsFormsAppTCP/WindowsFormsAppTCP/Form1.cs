bdf7a2009f6f9d8d33ec36045073018bcf2ed161
chyileee <chyileee@gmail.com>
2019-01-07 22:35:28 +0800

    Client Server programming in C# (Chat application) origin

---
+++ DotNet_LibraryTutorial/WindowsFormsAppTCP/WindowsFormsAppTCP/Form1.cs
@@ -0,0 +1,32 @@
+﻿//https://www.youtube.com/watch?v=X16IyNbcAr0
+//Client Server programming in C# (Chat application)
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsAppTCP
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}

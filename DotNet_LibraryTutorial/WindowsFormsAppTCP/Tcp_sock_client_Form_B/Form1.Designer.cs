570fe1a48b367309370a3b1df32e048701250fd4
chyi <chyileee@gmail.com>
2019-01-11 18:42:46 +0800

    connected to server error

--- DotNet_LibraryTutorial/WindowsFormsAppTCP/Tcp_sock_client_Form_B/Form1.Designer.cs
+++ DotNet_LibraryTutorial/WindowsFormsAppTCP/Tcp_sock_client_Form_B/Form1.Designer.cs
@@ -31,17 +31,18 @@
             this.ConnectBtn = new System.Windows.Forms.Button();
             this.Server_IP_label = new System.Windows.Forms.Label();
             this.Server_groupBox = new System.Windows.Forms.GroupBox();
-            this.Server_Port_label = new System.Windows.Forms.Label();
-            this.Server_IP_textBox = new System.Windows.Forms.TextBox();
             this.Server_Port_textBox = new System.Windows.Forms.TextBox();
+            this.Server_IP_textBox = new System.Windows.Forms.TextBox();
+            this.Server_Port_label = new System.Windows.Forms.Label();
             this.Client_groupBox = new System.Windows.Forms.GroupBox();
-            this.Send_button = new System.Windows.Forms.Button();
-            this.Client_IP_label = new System.Windows.Forms.Label();
-            this.Client_Port_label = new System.Windows.Forms.Label();
-            this.Client_IP_textBox = new System.Windows.Forms.TextBox();
-            this.Client_Port_textBox = new System.Windows.Forms.TextBox();
-            this.Message_textBox = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.Message_textBox = new System.Windows.Forms.TextBox();
+            this.Client_Port_textBox = new System.Windows.Forms.TextBox();
+            this.Client_IP_textBox = new System.Windows.Forms.TextBox();
+            this.Client_Port_label = new System.Windows.Forms.Label();
+            this.Client_IP_label = new System.Windows.Forms.Label();
+            this.Send_button = new System.Windows.Forms.Button();
+            this.Message_listBox = new System.Windows.Forms.ListBox();
             this.Server_groupBox.SuspendLayout();
             this.Client_groupBox.SuspendLayout();
             this.SuspendLayout();
@@ -54,6 +55,7 @@
             this.ConnectBtn.TabIndex = 0;
             this.ConnectBtn.Text = "Connect to Server";
             this.ConnectBtn.UseVisualStyleBackColor = true;
+            this.ConnectBtn.Click += new System.EventHandler(this.ConnectBtn_Click);
             // 
             // Server_IP_label
             // 
@@ -77,6 +79,22 @@
             this.Server_groupBox.TabIndex = 2;
             this.Server_groupBox.TabStop = false;
             this.Server_groupBox.Text = "Remote(be_a_Server)";
+            this.Server_groupBox.Enter += new System.EventHandler(this.Server_groupBox_Enter);
+            // 
+            // Server_Port_textBox
+            // 
+            this.Server_Port_textBox.Location = new System.Drawing.Point(64, 59);
+            this.Server_Port_textBox.Name = "Server_Port_textBox";
+            this.Server_Port_textBox.Size = new System.Drawing.Size(100, 22);
+            this.Server_Port_textBox.TabIndex = 4;
+            // 
+            // Server_IP_textBox
+            // 
+            this.Server_IP_textBox.Location = new System.Drawing.Point(64, 30);
+            this.Server_IP_textBox.Name = "Server_IP_textBox";
+            this.Server_IP_textBox.Size = new System.Drawing.Size(190, 22);
+            this.Server_IP_textBox.TabIndex = 3;
+            this.Server_IP_textBox.TextChanged += new System.EventHandler(this.Server_IP_textBox_TextChanged);
             // 
             // Server_Port_label
             // 
@@ -86,20 +104,6 @@
             this.Server_Port_label.Size = new System.Drawing.Size(34, 12);
             this.Server_Port_label.TabIndex = 2;
             this.Server_Port_label.Text = "PORT";
-            // 
-            // Server_IP_textBox
-            // 
-            this.Server_IP_textBox.Location = new System.Drawing.Point(64, 30);
-            this.Server_IP_textBox.Name = "Server_IP_textBox";
-            this.Server_IP_textBox.Size = new System.Drawing.Size(190, 22);
-            this.Server_IP_textBox.TabIndex = 3;
-            // 
-            // Server_Port_textBox
-            // 
-            this.Server_Port_textBox.Location = new System.Drawing.Point(64, 59);
-            this.Server_Port_textBox.Name = "Server_Port_textBox";
-            this.Server_Port_textBox.Size = new System.Drawing.Size(100, 22);
-            this.Server_Port_textBox.TabIndex = 4;
             // 
             // Client_groupBox
             // 
@@ -117,23 +121,36 @@
             this.Client_groupBox.TabStop = false;
             this.Client_groupBox.Text = "Local(be_a_Client)";
             // 
-            // Send_button
+            // label1
             // 
-            this.Send_button.Location = new System.Drawing.Point(278, 93);
-            this.Send_button.Name = "Send_button";
-            this.Send_button.Size = new System.Drawing.Size(75, 23);
-            this.Send_button.TabIndex = 0;
-            this.Send_button.Text = "Send";
-            this.Send_button.UseVisualStyleBackColor = true;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 98);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 12);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Message";
             // 
-            // Client_IP_label
+            // Message_textBox
             // 
-            this.Client_IP_label.AutoSize = true;
-            this.Client_IP_label.Location = new System.Drawing.Point(43, 25);
-            this.Client_IP_label.Name = "Client_IP_label";
-            this.Client_IP_label.Size = new System.Drawing.Size(15, 12);
-            this.Client_IP_label.TabIndex = 1;
-            this.Client_IP_label.Text = "IP";
+            this.Message_textBox.Location = new System.Drawing.Point(64, 93);
+            this.Message_textBox.Name = "Message_textBox";
+            this.Message_textBox.Size = new System.Drawing.Size(190, 22);
+            this.Message_textBox.TabIndex = 5;
+            this.Message_textBox.Text = "Message_to_send";
+            // 
+            // Client_Port_textBox
+            // 
+            this.Client_Port_textBox.Location = new System.Drawing.Point(64, 50);
+            this.Client_Port_textBox.Name = "Client_Port_textBox";
+            this.Client_Port_textBox.Size = new System.Drawing.Size(100, 22);
+            this.Client_Port_textBox.TabIndex = 4;
+            // 
+            // Client_IP_textBox
+            // 
+            this.Client_IP_textBox.Location = new System.Drawing.Point(64, 22);
+            this.Client_IP_textBox.Name = "Client_IP_textBox";
+            this.Client_IP_textBox.Size = new System.Drawing.Size(190, 22);
+            this.Client_IP_textBox.TabIndex = 3;
             // 
             // Client_Port_label
             // 
@@ -144,42 +161,39 @@
             this.Client_Port_label.TabIndex = 2;
             this.Client_Port_label.Text = "PORT";
             // 
-            // Client_IP_textBox
+            // Client_IP_label
             // 
-            this.Client_IP_textBox.Location = new System.Drawing.Point(64, 22);
-            this.Client_IP_textBox.Name = "Client_IP_textBox";
-            this.Client_IP_textBox.Size = new System.Drawing.Size(190, 22);
-            this.Client_IP_textBox.TabIndex = 3;
+            this.Client_IP_label.AutoSize = true;
+            this.Client_IP_label.Location = new System.Drawing.Point(43, 25);
+            this.Client_IP_label.Name = "Client_IP_label";
+            this.Client_IP_label.Size = new System.Drawing.Size(15, 12);
+            this.Client_IP_label.TabIndex = 1;
+            this.Client_IP_label.Text = "IP";
             // 
-            // Client_Port_textBox
+            // Send_button
             // 
-            this.Client_Port_textBox.Location = new System.Drawing.Point(64, 50);
-            this.Client_Port_textBox.Name = "Client_Port_textBox";
-            this.Client_Port_textBox.Size = new System.Drawing.Size(100, 22);
-            this.Client_Port_textBox.TabIndex = 4;
+            this.Send_button.Location = new System.Drawing.Point(278, 93);
+            this.Send_button.Name = "Send_button";
+            this.Send_button.Size = new System.Drawing.Size(75, 23);
+            this.Send_button.TabIndex = 0;
+            this.Send_button.Text = "Send";
+            this.Send_button.UseVisualStyleBackColor = true;
             // 
-            // Message_textBox
+            // Message_listBox
             // 
-            this.Message_textBox.Location = new System.Drawing.Point(64, 93);
-            this.Message_textBox.Name = "Message_textBox";
-            this.Message_textBox.Size = new System.Drawing.Size(190, 22);
-            this.Message_textBox.TabIndex = 5;
-            this.Message_textBox.Text = "Message_to_send";
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(14, 98);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(44, 12);
-            this.label1.TabIndex = 6;
-            this.label1.Text = "Message";
+            this.Message_listBox.FormattingEnabled = true;
+            this.Message_listBox.ItemHeight = 12;
+            this.Message_listBox.Location = new System.Drawing.Point(22, 305);
+            this.Message_listBox.Name = "Message_listBox";
+            this.Message_listBox.Size = new System.Drawing.Size(437, 220);
+            this.Message_listBox.TabIndex = 4;
             // 
             // Tcp_sock
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(485, 308);
+            this.ClientSize = new System.Drawing.Size(485, 556);
+            this.Controls.Add(this.Message_listBox);
             this.Controls.Add(this.Client_groupBox);
             this.Controls.Add(this.Server_groupBox);
             this.Name = "Tcp_sock";
@@ -208,6 +222,7 @@
         private System.Windows.Forms.Label Client_Port_label;
         private System.Windows.Forms.Label Client_IP_label;
         private System.Windows.Forms.Button Send_button;
+        private System.Windows.Forms.ListBox Message_listBox;
     }
 }
 
--- DotNet_LibraryTutorial/WindowsFormsAppTCP/Tcp_sock_client_Form_B/Form1.cs
+++ DotNet_LibraryTutorial/WindowsFormsAppTCP/Tcp_sock_client_Form_B/Form1.cs
@@ -1,20 +1,92 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
 using System.Windows.Forms;
 
 namespace Tcp_sock_client_Form_B
 {
     public partial class Tcp_sock : Form
     {
+        Socket sck;
+        EndPoint epLocal, epRemote;
         public Tcp_sock()
         {
+            int Port1 = 8080;
+            int Port2 = 9000;
             InitializeComponent();
+            Server_IP_textBox.Text = GetLocalIP();
+            Server_Port_textBox.Text = Port1.ToString();
+            Client_IP_textBox.Text = GetLocalIP();
+            Client_Port_textBox.Text = Port2.ToString();
+            epLocal = new IPEndPoint(IPAddress.Parse(Client_IP_textBox.Text), Convert.ToInt32(Client_Port_textBox.Text));
+            epRemote = new IPEndPoint(IPAddress.Parse(Server_IP_textBox.Text), Convert.ToInt32(Server_Port_textBox.Text));
+
+            Socket sck = new Socket(epRemote.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            sck.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
+            Message_listBox.Items.Add("Tcp_sock_client_A.cs");
+            Message_listBox.Items.Add("HELLO");
+
+        }
+
+        private string GetLocalIP()
+        {
+            IPHostEntry host;
+            host = Dns.GetHostEntry(Dns.GetHostName());
+
+            foreach (IPAddress ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    return ip.ToString();
+                }
+            }
+
+            return "127.0.0.1";
+        }
+
+
+
+        private void Server_groupBox_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Server_IP_textBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ConnectBtn_Click(object sender, EventArgs e)
+        {
+               try
+                {
+                sck.Bind(epLocal);
+                sck.Connect(epRemote);
+
+
+                Message_listBox.Items.Add("Tcp Client => Tcp_sock_client_A.cs");
+                Message_listBox.Items.Add("LocalEndPoint = ");
+                Message_listBox.Items.Add(sck.LocalEndPoint.ToString());
+                Message_listBox.Items.Add("Connected to Server = ");
+                Message_listBox.Items.Add(sck.RemoteEndPoint.ToString());
+
+                //byte[] sendmsg = Encoding.ASCII.GetBytes("This is from Client\n");
+
+                //int n = client.Send(sendmsg);
+
+                //int m = client.Receive(data);
+
+                //Console.WriteLine("" + Encoding.ASCII.GetString(data));
+                //client.Shutdown(SocketShutdown.Both);
+                //client.Close();
+            }
+            catch (Exception ex)
+            {
+                Message_listBox.Items.Add(ex.ToString());
+
+            }
+
+
         }
     }
 }
